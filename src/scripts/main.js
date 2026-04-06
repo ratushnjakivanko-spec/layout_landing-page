a3ce4f5eeeb44441ec91d68e798631078d3c71c0
Ivan Ratushnjak <ratushnjak.ivanko@gmail.com>
2026-04-06 14:38:14 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,37 @@
 'use strict';
+
+import '../styles/main.scss';
+
+const hamburger = document.getElementById('hamburger');
+const menuPanel = document.getElementById('menuPanel');
+const menuOverlay = document.getElementById('menuOverlay');
+const menuClose = document.getElementById('menuClose');
+
+function openMenu() {
+  hamburger.classList.add('open');
+  menuPanel.classList.add('open');
+  menuOverlay.classList.add('open');
+  document.body.style.overflow = 'hidden';
+}
+
+function closeMenu() {
+  hamburger.classList.remove('open');
+  menuPanel.classList.remove('open');
+  menuOverlay.classList.remove('open');
+  document.body.style.overflow = '';
+}
+
+hamburger.addEventListener('click', () => {
+  if (menuPanel.classList.contains('open')) {
+    closeMenu();
+  } else {
+    openMenu();
+  }
+});
+
+menuClose.addEventListener('click', closeMenu);
+menuOverlay.addEventListener('click', closeMenu);
+
+document.querySelectorAll('.menu-link').forEach((link) => {
+  link.addEventListener('click', closeMenu);
+});
